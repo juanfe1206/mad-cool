b71ce52c1e6bf88acfbb854cdb94e1cc4ab66acd
juanfe1206 <juanfeortizlejarde@gmail.com>
2024-11-22 01:02:57 +0100

    started person behaviour

--- Person.py
+++ Person.py
@@ -15,7 +15,7 @@ class Person(Festival):
     self.hunger = random.randint(0, 5)
     self.thirst = random.randint(0, 5)
     self.need_bathroom = random.randint(0, 5)
-    self.busy = False
+    self.want_merch = random.randint(0, 7)
   
   def enter_festival(self):
     self.is_inside = True
@@ -33,6 +33,40 @@ class Person(Festival):
       pass
     if small_stage_2_artist in self.minor_artist_preference:
       pass
+  
+  def go_eat(self):
+    pass
+  
+  def go_drink(self):
+    pass
+    
+  def behaviour(self, festival):
+    while True:
+      #Exit condition
+      if festival.festival_finished == True:
+        break
+      
+      if self.hunger > 7:
+        self.go_eat()
+        continue
+      
+      if self.thirst > 7:
+        self.go_drink()
+        continue
+      
+      if self.need_bathroom > 7:
+        self.go_to_bathroom()
+        
+      if self.want_merch > 8:
+        self.go_buy_merch()
+        
+      #Now check what artist is playing and if I want to see them... 
+      #If I do, get stage and add myself to the concert and build concert behaviour to increase the other variables
+      #If I don't I will do one of the other activities based on probabilities.
+        
+      
+      
+    
   
 
     
